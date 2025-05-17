e6a9732d537f4b48070a80d16ed2606fa0b83802
odsihgioudsh <49092646+odsihgioudsh@users.noreply.github.com>
2025-05-17 23:45:13 +0800

    test

--- zipalign_sign.sh
+++ zipalign_sign.sh
@@ -3,7 +3,7 @@ PATH=$PATH:$ANDROID_HOME/build-tools/32.0.0/
 for f in build/*.apk; do
     mv $f ${f%.apk}.apk.unsigned
     echo "Zipaligning $f"
-    ./zipalign -pvf 4 ${f%.apk}.apk.unsigned $f
+    $ANDROID_HOME/build-tools/32.0.0/zipalign -pvf 4 ${f%.apk}.apk.unsigned $f
     rm ${f%.apk}.apk.unsigned
     echo "Signing $f"
     echo $(apksigner --version)
