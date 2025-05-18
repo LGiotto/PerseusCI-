16ff9dc38f2fa2bd99e5294ab5366dfee655bf35
odsihgioudsh <49092646+odsihgioudsh@users.noreply.github.com>
2025-05-18 18:31:36 +0800

    1

--- zipalign_sign.sh
+++ zipalign_sign.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 PATH=$PATH:$ANDROID_HOME/build-tools/32.0.0/
 for f in build/*.apk; do
-    if test -e $ANDROID_HOME/build-tools
-    then
-        echo '文件已存在!'
-    else
-        echo '文件不存在!'
-    fi
+    ls $ANDROID_HOME/build-tools
     mv $f ${f%.apk}.apk.unsigned
     echo "Zipaligning $f"
     zipalign -pvf 4 ${f%.apk}.apk.unsigned $f
